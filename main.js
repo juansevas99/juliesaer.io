e7a2619bcc71348421bdc3d535fd4e9a1b69c42c
JuGALVIS <66051369+JuGALVIS@users.noreply.github.com>
2020-09-22 13:32:31 -0500

    primer commit de prueba

---
+++ main.js
@@ -0,0 +1,10 @@
+function ajustarTamaño(){
+    var panelIzquierda = document.getElementById("em_1");
+    var panelDerecha = document.getElementById("PanelDerecha");
+    
+    console.log(panelIzquierda.clientWidth);
+    console.log(panelIzquierda.clientHeight);
+    
+    console.log(panelDerecha.clientWidth);
+    console.log(panelDerecha.clientHeight);
+  }
